46c5a6378ed86c2069bad091114e84180a3a4792
Robbie Ferguson <contact@category5.tv>
2021-02-16 16:37:03 -0500

    Fix version variable, begin work on PSI

---
+++ assets/psi.php
@@ -0,0 +1,21 @@
+<?php
+  /*
+    Pinecraft Settings Interface (PSI)
+    By Robbie Ferguson
+  */
+?>
+<!DOCTYPE html><html lang="en"><head><meta charset="utf-8" /><title>Pinecraft</title>
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+<style>
+body {
+  background: #333;
+  color: #ccc;
+  font-family: 'Open Sans', sans-serif;
+}
+</style>
+</head>
+<body>
+  This is not yet implemented. Check back in a future release.
+</body>
+</html>
