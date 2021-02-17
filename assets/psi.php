6faf94895e908cf36099f5f01566c24a7813b4e3
Robbie Ferguson <contact@category5.tv>
2021-02-17 14:28:59 -0500

    Integrate Bootstrap in PSI

--- assets/psi.php
+++ assets/psi.php
@@ -5,51 +5,310 @@
   */
   $cfgfile = '/etc/pinecraft/psi/psi.json';
   $config = json_decode(file_get_contents($cfgfile));
-  if (!isset($config->server)) {
-    if (file_exists($config->instdir . 'server.properties')) {
-      $server = file($config->instdir . 'server.properties');
-      if (is_array($server)) {
-        $config->server = new stdClass();
-        foreach($server as $line) {
-          // Ignore commented lines entirely
-          if (substr(trim($line),0,1) != '#') {
-            $tmp = explode('=',$line);
-            $key = trim($tmp[0]);
-            $value = trim($tmp[1]);
-            $config->server->$key = $value;
-          }
+
+  // Load Game Server Version
+  if (file_exists($config->instdir . 'version_history.json')) {
+    $tmp = json_decode(file_get_contents($config->instdir . 'version_history.json'));
+    $config->mcver = $tmp->currentVersion;
+  }
+
+  // Load server.properties
+  if (file_exists($config->instdir . 'server.properties')) {
+    $server = file($config->instdir . 'server.properties');
+    if (is_array($server)) {
+      $config->server = new stdClass();
+      foreach($server as $line) {
+        // Ignore commented lines entirely
+        if (substr(trim($line),0,1) != '#') {
+          $tmp = explode('=',$line);
+          $key = trim($tmp[0]);
+          $value = trim($tmp[1]);
+          $config->server->$key = $value;
         }
       }
-    } else {
-      die('Server not initialized.');
     }
+  } else {
+    die('Server not initialized.');
   }
+
+  // Load user info
+  if (!isset($config->server->users)) $config->server->users = new stdClass();
+  if (!isset($config->server->users->ban)) $config->server->users->ban = new stdClass();
+  // Players on server
+  if (file_exists($config->instdir . 'usercache.json')) {
+    $config->server->users->players = json_decode(file_get_contents($config->instdir . 'usercache.json'));
+  }
+  // Ops (admin) players
+  if (file_exists($config->instdir . 'ops.json')) {
+    $config->server->users->ops = json_decode(file_get_contents($config->instdir . 'ops.json'));
+  }
+  // Banned players
+  if (file_exists($config->instdir . 'banned-players.json')) {
+    $config->server->users->ban->player = json_decode(file_get_contents($config->instdir . 'banned-players.json'));
+  }
+  // Banned IP addresses
+  if (file_exists($config->instdir . 'banned-ips.json')) {
+    $config->server->users->ban->ip = json_decode(file_get_contents($config->instdir . 'banned-ips.json'));
+  }
+
+  // Store everything in the main config file
   file_put_contents($cfgfile,json_encode($config));
+
   $load = sys_getloadavg();
 ?>
-<!DOCTYPE html><html lang="en"><head><meta charset="utf-8" /><title>Pinecraft</title>
-<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
-<style>
-body {
-  background: #333;
-  color: #ccc;
-  font-family: 'Open Sans', sans-serif;
-  font-size: 16px;
-}
-#topbar {
-  font-weight: bold;
-  font-size: 1.2em;
-  color: #fff;
-  padding: 4px;
-}
-</style>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Pinecraft</title>
+  <meta name="author" content="Robbie Ferguson">
+
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <meta name="theme-color" content="#7952b3">
+  <style>
+    body {
+      font-family: 'Open Sans', sans-serif;
+    }
+    .container {
+      max-width: 960px;
+    }
+  </style>
 </head>
-<body>
-  <div id="topbar">
-    <p>Pinecraft Settings Interface</p>
-    <p>Server Load: <?= $load[1] ?></p>
+<body class="bg-light">
+  <div id="topbar" class="container">
+    <div class="row">
+      <div class="col-md-6 text-start fw-bold fs-3">Pinecraft Settings Interface</div>
+      <div class="col-md-6 text-end fs-5 font-monospace">Server Load: <?= $load[1] ?></div>
+    </div>
   </div>
-  <p>Coming soon.<?php print_r($config) ?></p>
+  <p><?php print_r($config) ?></p>
+
+<div class="container">
+  <main>
+    <div class="py-5 text-center">
+      <img class="d-block mx-auto mb-4" src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+      <h2>Checkout form</h2>
+      <p class="lead">Below is an example form built entirely with Bootstrap’s form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.</p>
+    </div>
+
+    <div class="row g-3">
+      <div class="col-md-5 col-lg-4 order-md-last">
+        <h4 class="d-flex justify-content-between align-items-center mb-3">
+          <span class="text-muted">Your cart</span>
+          <span class="badge bg-secondary rounded-pill">3</span>
+        </h4>
+        <ul class="list-group mb-3">
+          <li class="list-group-item d-flex justify-content-between lh-sm">
+            <div>
+              <h6 class="my-0">Product name</h6>
+              <small class="text-muted">Brief description</small>
+            </div>
+            <span class="text-muted">$12</span>
+          </li>
+          <li class="list-group-item d-flex justify-content-between lh-sm">
+            <div>
+              <h6 class="my-0">Second product</h6>
+              <small class="text-muted">Brief description</small>
+            </div>
+            <span class="text-muted">$8</span>
+          </li>
+          <li class="list-group-item d-flex justify-content-between lh-sm">
+            <div>
+              <h6 class="my-0">Third item</h6>
+              <small class="text-muted">Brief description</small>
+            </div>
+            <span class="text-muted">$5</span>
+          </li>
+          <li class="list-group-item d-flex justify-content-between bg-light">
+            <div class="text-success">
+              <h6 class="my-0">Promo code</h6>
+              <small>EXAMPLECODE</small>
+            </div>
+            <span class="text-success">−$5</span>
+          </li>
+          <li class="list-group-item d-flex justify-content-between">
+            <span>Total (USD)</span>
+            <strong>$20</strong>
+          </li>
+        </ul>
+
+        <form class="card p-2">
+          <div class="input-group">
+            <input type="text" class="form-control" placeholder="Promo code">
+            <button type="submit" class="btn btn-secondary">Redeem</button>
+          </div>
+        </form>
+      </div>
+      <div class="col-md-7 col-lg-8">
+        <h4 class="mb-3">Billing address</h4>
+        <form class="needs-validation" novalidate>
+          <div class="row g-3">
+            <div class="col-sm-6">
+              <label for="firstName" class="form-label">First name</label>
+              <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
+              <div class="invalid-feedback">
+                Valid first name is required.
+              </div>
+            </div>
+
+            <div class="col-sm-6">
+              <label for="lastName" class="form-label">Last name</label>
+              <input type="text" class="form-control" id="lastName" placeholder="" value="" required>
+              <div class="invalid-feedback">
+                Valid last name is required.
+              </div>
+            </div>
+
+            <div class="col-12">
+              <label for="username" class="form-label">Username</label>
+              <div class="input-group has-validation">
+                <span class="input-group-text">@</span>
+                <input type="text" class="form-control" id="username" placeholder="Username" required>
+              <div class="invalid-feedback">
+                  Your username is required.
+                </div>
+              </div>
+            </div>
+
+            <div class="col-12">
+              <label for="email" class="form-label">Email <span class="text-muted">(Optional)</span></label>
+              <input type="email" class="form-control" id="email" placeholder="you@example.com">
+              <div class="invalid-feedback">
+                Please enter a valid email address for shipping updates.
+              </div>
+            </div>
+
+            <div class="col-12">
+              <label for="address" class="form-label">Address</label>
+              <input type="text" class="form-control" id="address" placeholder="1234 Main St" required>
+              <div class="invalid-feedback">
+                Please enter your shipping address.
+              </div>
+            </div>
+
+            <div class="col-12">
+              <label for="address2" class="form-label">Address 2 <span class="text-muted">(Optional)</span></label>
+              <input type="text" class="form-control" id="address2" placeholder="Apartment or suite">
+            </div>
+
+            <div class="col-md-5">
+              <label for="country" class="form-label">Country</label>
+              <select class="form-select" id="country" required>
+                <option value="">Choose...</option>
+                <option>United States</option>
+              </select>
+              <div class="invalid-feedback">
+                Please select a valid country.
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <label for="state" class="form-label">State</label>
+              <select class="form-select" id="state" required>
+                <option value="">Choose...</option>
+                <option>California</option>
+              </select>
+              <div class="invalid-feedback">
+                Please provide a valid state.
+              </div>
+            </div>
+
+            <div class="col-md-3">
+              <label for="zip" class="form-label">Zip</label>
+              <input type="text" class="form-control" id="zip" placeholder="" required>
+              <div class="invalid-feedback">
+                Zip code required.
+              </div>
+            </div>
+          </div>
+
+          <hr class="my-4">
+
+          <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="same-address">
+            <label class="form-check-label" for="same-address">Shipping address is the same as my billing address</label>
+          </div>
+
+          <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="save-info">
+            <label class="form-check-label" for="save-info">Save this information for next time</label>
+          </div>
+
+          <hr class="my-4">
+
+          <h4 class="mb-3">Payment</h4>
+
+          <div class="my-3">
+            <div class="form-check">
+              <input id="credit" name="paymentMethod" type="radio" class="form-check-input" checked required>
+              <label class="form-check-label" for="credit">Credit card</label>
+            </div>
+            <div class="form-check">
+              <input id="debit" name="paymentMethod" type="radio" class="form-check-input" required>
+              <label class="form-check-label" for="debit">Debit card</label>
+            </div>
+            <div class="form-check">
+              <input id="paypal" name="paymentMethod" type="radio" class="form-check-input" required>
+              <label class="form-check-label" for="paypal">PayPal</label>
+            </div>
+          </div>
+
+          <div class="row gy-3">
+            <div class="col-md-6">
+              <label for="cc-name" class="form-label">Name on card</label>
+              <input type="text" class="form-control" id="cc-name" placeholder="" required>
+              <small class="text-muted">Full name as displayed on card</small>
+              <div class="invalid-feedback">
+                Name on card is required
+              </div>
+            </div>
+
+            <div class="col-md-6">
+              <label for="cc-number" class="form-label">Credit card number</label>
+              <input type="text" class="form-control" id="cc-number" placeholder="" required>
+              <div class="invalid-feedback">
+                Credit card number is required
+              </div>
+            </div>
+
+            <div class="col-md-3">
+              <label for="cc-expiration" class="form-label">Expiration</label>
+              <input type="text" class="form-control" id="cc-expiration" placeholder="" required>
+              <div class="invalid-feedback">
+                Expiration date required
+              </div>
+            </div>
+
+            <div class="col-md-3">
+              <label for="cc-cvv" class="form-label">CVV</label>
+              <input type="text" class="form-control" id="cc-cvv" placeholder="" required>
+              <div class="invalid-feedback">
+                Security code required
+              </div>
+            </div>
+          </div>
+
+          <hr class="my-4">
+
+          <button class="w-100 btn btn-primary btn-lg" type="submit">Continue to checkout</button>
+        </form>
+      </div>
+    </div>
+  </main>
+
+  <footer class="my-5 pt-5 text-muted text-center text-small">
+    <p class="mb-1">&copy; 2017–2021 Company Name</p>
+    <ul class="list-inline">
+      <li class="list-inline-item"><a href="#">Privacy</a></li>
+      <li class="list-inline-item"><a href="#">Terms</a></li>
+      <li class="list-inline-item"><a href="#">Support</a></li>
+    </ul>
+  </footer>
+</div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
 </html>
